cc5e21232056c6e416fa0d3f534a7486b7fa81b0
doyoon <dyniiyeyo@naver.com>
2025-07-24 18:33:03 +0900

    jungol input practice

---
+++ src/jungol/입력/입력_연습문제1.java
@@ -0,0 +1,10 @@
+package jungol.입력;
+
+public class 입력_연습문제1 {
+    public static void main(String[] args) {
+        int a = 10;
+        String b = "A";
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
---
+++ src/jungol/입력/입력_형성평가2.java
@@ -0,0 +1,10 @@
+package jungol.입력;
+
+public class 입력_형성평가2 {
+    public static void main(String[] args) {
+        float a = 80.5f;
+        float b = 22.34f;
+
+        System.out.printf("     %.2f     %.2f    %.2f", a, b, a+b);
+    }
+}
